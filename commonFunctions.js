13ee50347aac05225e818efe880b7bca44e18afe
Martín Ferrari <martinferra@gmail.com>
2021-08-02 21:05:24 -0300

    Commit inicial

---
+++ commonFunctions.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const lodash = require('lodash');
+const cloneDeep = lodash.cloneDeep;
+
+function expandRegEx(regEx) {
+    return regEx
+        .replace(/a|á/ig, '[aá]')
+        .replace(/e|é/ig, '[eé]')
+        .replace(/i|í/ig, '[ií]')
+        .replace(/o|ó/ig, '[oó]')
+        .replace(/u|ú|ü/ig, '[uúü]')
+        .replace(/n|ñ/ig, '[nñ]')
+}
+
+function getRegEx(queryStr, phrase=false) {
+
+    let joinStr = phrase? '.*' : '|'
+
+    return queryStr
+        .split(' ')
+        .filter(term=>term.length>0)
+        .map(expandRegEx)
+        .join(joinStr)
+}
+
+function applyRecursive(parentObj, parentKey, obj, cb) {
+    for (var key in obj) {
+      if (!obj.hasOwnProperty(key)) {
+        continue;
+      }
+      if (typeof obj[key] == "object" && obj[key] !== null) {
+        applyRecursive(obj, key, obj[key], cb);
+      } else {
+        cb(parentObj, parentKey, obj, key);
+      }
+    }
+    return obj;
+  }
+  
+  function applyObjectIdRecursive(obj) {
+    clonedObj = cloneDeep(obj);
+    return applyRecursive(null, null, clonedObj, (po,pk,o,k)=>{
+      /* Ej: "customer: {$oid:'aaaaa'}" se reemplaza por 
+         "customer: ObjectId('aaaaa')" */
+      if(k==='$oid' && typeof o[k] === 'string') {
+        po[pk] = mongoose.Types.ObjectId(o[k]);
+      }
+    })
+  }
+
+module.exports = {
+    expandRegEx,
+    getRegEx,
+    applyRecursive,
+    applyObjectIdRecursive
+}
