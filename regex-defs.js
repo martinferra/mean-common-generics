967a49abfff30cad3935ab07c876d545a59a1ec6
Martín Ferrari <martinferra@gmail.com>
2022-11-13 04:01:42 -0300

    Nuevo módulo de definición de expresiones regulares Fixes #4

---
+++ regex-defs.js
@@ -0,0 +1,6 @@
+const RegExDefs = {
+    // Regexp to validate emails with more strict rules as added in tests/users.js which also conforms mostly with RFC2822 guide lines
+    email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+}
+
+module.exports = RegExDefs;
