00d6b57cb0c5a7cf5e883fa944e5b40fcc0793ee
Martín Ferrari <martinferra@gmail.com>
2023-11-19 03:27:51 -0300

    Nuevas funciones para generación de nombres de archivos Fixes #5

--- commonFunctions.d.ts
+++ commonFunctions.d.ts
@@ -2,3 +2,6 @@ export function expandRegEx(regEx: any): any;
 export function getRegEx(queryStr: any, phrase?: boolean): any;
 export function applyRecursive(parentObj: any, parentKey: any, obj: any, cb: any): any;
 export function replaceById(obj: any): string;
+export function dateToFilenameSuffix(date: Date): string;
+export function stringToFilenameSuffix(str: string): string;
+export function getTimestampString(): string;
--- commonFunctions.js
+++ commonFunctions.js
@@ -1,5 +1,3 @@
-const moment = require('moment');
-
 function expandRegEx(regEx) {
   return regEx
     .replace(/a|á/ig, '[aá]')
@@ -43,9 +41,43 @@ function replaceById(obj) {
   }
 }
 
+function dateToFilenameSuffix(date) {
+  let year = date.getFullYear();
+  let month = (date.getMonth() + 1).toString().padStart(2, '0');
+  let day = date.getDate().toString().padStart(2, '0');
+  let hours = date.getHours().toString().padStart(2, '0');
+  let minutes = date.getMinutes().toString().padStart(2, '0');
+  let seconds = date.getSeconds().toString().padStart(2, '0');
+
+  return `${year}${month}${day}${hours}${minutes}${seconds}`;
+};
+
+function getTimestampString() {
+  return dateToFilenameSuffix(new Date());
+}
+
+function stringToFilenameSuffix(inputString) {
+  // Lowercase all characters
+  let formattedString = inputString.toLowerCase();
+
+  // Replace spaces or sequence of spaces with "_"
+  formattedString = formattedString.replace(/\s+/g, '_');
+
+  // Replace accent characters with non-accent versions
+  formattedString = formattedString.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+  // Exclude non-ASCII characters
+  formattedString = formattedString.replace(/[^\x00-\x7F]/g, "");
+
+  return formattedString;
+}
+
 module.exports = {
   expandRegEx,
   getRegEx,
   applyRecursive,
   replaceById,
+  dateToFilenameSuffix,
+  getTimestampString,
+  stringToFilenameSuffix
 }
